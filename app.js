ad8e0c525876624ca8615c2c80f0694adfa84a4d
Naglapur <rnaglapur@informatica.com>
2018-12-17 17:33:57 +0530

    Initial angularjs setup

---
+++ app.js
@@ -0,0 +1,7 @@
+// MODULE definition
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// weatherApp.config(function($routeProvider) {
+//     $routeProvider
+//     .when('\').
+// })
