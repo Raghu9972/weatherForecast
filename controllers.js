edf9902ed8436ad15191adb5a0d881b6c990b1ed
Naglapur <rnaglapur@informatica.com>
2018-12-18 17:58:58 +0530

    Code cleanup: Seperated API calls from controller and made a seperate service

--- controllers.js
+++ controllers.js
@@ -10,20 +10,11 @@ weatherApp.controller('homeController', ['$scope', '$location', 'cityService', f
     }
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource', '$routeParams', function ($scope, cityService, $resource, $routeParams) {
+weatherApp.controller('forecastController', ['$scope', 'cityService', 'weatherService', '$routeParams', function ($scope, cityService, weatherService, $routeParams) {
     $scope.city = cityService.city || '';
     $scope.days = $routeParams.days || '2';
 
-    // http://api.openweathermap.org/data/2.5/forecast?q=London,uk&APPID=15efb2fb717febc51663415f77131342&cnt=2
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?APPID=15efb2fb717febc51663415f77131342", {
-        callback: "JSON_CALLBACK"
-    }, {
-        get: {
-            method: "JSONP"
-        }
-    });
-
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    $scope.weatherResult = weatherService.getWeather($scope.city, $scope.days);
 
     $scope.temperatureKelvinToCelsius = function(k) {
         return (k-273).toFixed(2);
--- services.js
+++ services.js
@@ -2,3 +2,20 @@
 weatherApp.service('cityService', function () {
     this.city = "Bangalore, IN";
 });
+
+weatherApp.service('weatherService', ['$resource', function ($resource) {
+    this.getWeather = function (city, days) {
+        // http://api.openweathermap.org/data/2.5/forecast?q=London,uk&APPID=15efb2fb717febc51663415f77131342&cnt=2
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?APPID=15efb2fb717febc51663415f77131342", {
+            callback: "JSON_CALLBACK"
+        }, {
+            get: {
+                method: "JSONP"
+            }
+        });
+        return weatherAPI.get({
+            q: city,
+            cnt: days
+        });
+    }
+}]);
